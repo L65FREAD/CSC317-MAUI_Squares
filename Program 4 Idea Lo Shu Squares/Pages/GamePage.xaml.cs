c006ab2dc377af6fef0523a04938862f1b226171
L65FREAD <l65.fread@gmail.com>
2023-11-29 00:37:39 -0600

    bug fixes

--- Program 4 Idea Lo Shu Squares/Pages/GamePage.xaml.cs
+++ Program 4 Idea Lo Shu Squares/Pages/GamePage.xaml.cs
@@ -13,4 +13,26 @@ public partial class GamePage : ContentPage
 		//This passes the game mode selection from the main menu to the game page. 
 		this.mode = mode;
 	}
+
+    void CellInput(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+    }
+    void OnMainMenuClicked(System.Object sender, System.EventArgs e)
+    { 
+        Application.Current.MainPage = new MainMenuPage(); 
+    }
+
+    void OnNewGameClicked(System.Object sender, System.EventArgs e)
+    {
+    }
+    void OnHintClicked(System.Object sender, System.EventArgs e)
+    {
+    }
+    void OnGridButtonClicked(System.Object sender, System.EventArgs e)
+    {
+    }
+    void OnGetHintClicked(System.Object sender, System.EventArgs e)
+    {
+    }
+    
 }
